ecfae7bee64878d097da2f376da2ece2c59172c6
mleijon <mikael.leijon@gmail.com>
2020-12-03 19:28:10 +0100

    Day3!

--- day3/day3b.py
+++ day3/day3b.py
@@ -3,7 +3,6 @@ def race(hor_stp, vert_stp):
         col = 0
         count = 0
         for i, line in enumerate(fi):
-            print(i, not i % vert_stp)
             if line[col % 31] == '#' and not i % vert_stp:
                 count += 1
             col += hor_stp*(not i % vert_stp)
