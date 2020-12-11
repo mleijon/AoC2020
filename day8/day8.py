b304b2ed8c53e45d2b98038555c40143bfb0f01b
mleijon <mikael.leijon@gmail.com>
2020-12-11 11:18:51 +0100

    Day8!

--- day8/day8.py
+++ day8/day8.py
@@ -1,19 +1,43 @@
+import copy
 operations = dict()
-accumulator = op_nr = 0
+op_nr = 0
 with open('p8_input.txt') as fi:
     for i, line in enumerate(fi.readlines()):
         operations[i] = [line.split(' ')[0], int(line.split(' ')[1].strip()),
                          False]
-while True:
-    if operations[op_nr][2]:
-        print('The answer to part 1 is: {}'.format(accumulator))
-        break
-    else:
-        operations[op_nr][2] = True
-        if operations[op_nr][0] == 'acc':
-            accumulator += operations[op_nr][1]
-            op_nr += 1
-        elif operations[op_nr][0] == 'nop':
-            op_nr += 1
+max_op_nr = len(operations)
+new_operations = copy.deepcopy(operations)
+
+
+def run_code(ops):
+    global max_op_nr
+    global op_nr
+    accumulator = 0
+    while True:
+        if op_nr == max_op_nr:
+            return accumulator
+        elif ops[op_nr][2]:
+            return accumulator
         else:
-            op_nr += operations[op_nr][1]
+            ops[op_nr][2] = True
+            if ops[op_nr][0] == 'acc':
+                accumulator += ops[op_nr][1]
+                op_nr += 1
+            elif ops[op_nr][0] == 'nop':
+                op_nr += 1
+            else:
+                op_nr += ops[op_nr][1]
+
+
+print('The answer to part 1 is: {}'.format(run_code(new_operations)))
+for key in operations.keys():
+    op_nr = 0
+    new_operations = copy.deepcopy(operations)
+    if operations[key][0] == 'nop':
+        new_operations[key][0] = 'jmp'
+    elif operations[key][0] == 'jmp':
+        new_operations[key][0] = 'nop'
+    acc = run_code(new_operations)
+    if op_nr == max_op_nr:
+        print('The answer to part 2 is: {}'.format(acc))
+        exit(0)
