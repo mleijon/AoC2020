1bc43f1892cb5debbe97b65987eaf8b9b85d3f64
mleijon <36968756+mleijon@users.noreply.github.com>
2020-12-17 11:05:10 +0100

    Day 12!

--- day12/day12.py
+++ day12/day12.py
@@ -10,59 +10,53 @@ def rot(dir, turn):
     ror = [(1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1)]
     rol = [(1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0), (-1, -1), (0, -1), (1, -1)]
     if stp == 'R':
-        newdir = ror[(ror.index(dir) + val//45) % 8]
+        return ror[(ror.index(dir) + val//45) % 8]
     else:
-        newdir = rol[(rol.index(dir) + val//45) % 8]
-    return newdir
+        return rol[(rol.index(dir) + val//45) % 8]
 
 
 def mvstp(mode, pos, dir):
-    if mode == 'move_ship':
-        coord = pos
-    else:
-        coord = dir
     for item in data:
         stp, val = item
         if stp == 'N':
             if mode == 'move_ship':
-                coord = list(map(add, coord, (0, val)))
+                pos = list(map(add, pos, (0, val)))
             else:
-                pass
+                dir = list(map(add, dir, (0, val)))
         elif stp == 'S':
             if mode == 'move_ship':
-                coord = list(map(add, coord, (0, -val)))
+                pos = list(map(add, pos, (0, -val)))
             else:
-                pass
+                dir = list(map(add, dir, (0, -val)))
         elif stp == 'W':
             if mode == 'move_ship':
-                coord = list(map(add, coord, (-val, 0)))
+                pos = list(map(add, pos, (-val, 0)))
             else:
-                pass
+                dir = list(map(add, dir, (-val, 0)))
         elif stp == 'E':
             if mode == 'move_ship':
-                coord = list(map(add, coord, (val, 0)))
+                pos = list(map(add, pos, (val, 0)))
             else:
-                pass
+                dir = list(map(add, dir, (val, 0)))
         elif stp == 'F':
-            if mode == 'move_ship':
-                coord = list(map(add, coord, list(map(mul, (val, val), dir))))
-            else:
-                pass
+            pos = list(map(add, pos, list(map(mul, (val, val), dir))))
         else:
             if 0 in dir:
-            #     dir = rot(dir, item)
-            # else:
-                dir_0m = abs(dir[0])
-                dir_0 = (int(dir[0]/dir_0m), 0)
-                dir_0 = rot(dir_0, item)
-                dir_0 = tuple(map(mul, (dir_0m, dir_0m), dir_0))
-                dir_1m = abs(dir[1])
-                dir_1 = (0, int(dir[1]/dir_1m))
-                dir_1 = rot(dir_1, item)
-                dir_1 = tuple(map(mul, (dir_1m, dir_1m), dir_1))
-                dir = tuple(map(add, dir_0, dir_1))
+                amp = max(abs(dir[0]), abs(dir[1]))
+                dir = (int(dir[0]/amp), int(dir[1]/amp))
+                dir = rot(dir, item)
+                dir = tuple(map(mul, (amp, amp), dir))
 
-    return abs(coord[0]) + abs(coord[1])
+            else:
+                dir_0 = (int(dir[0]/abs(dir[0])), 0)
+                dir_0 = rot(dir_0, item)
+                dir_0 = tuple(map(mul, (abs(dir[0]), abs(dir[0])), dir_0))
+                dir_1 = (0, int(dir[1]/abs(dir[1])))
+                dir_1 = rot(dir_1, item)
+                dir_1 = tuple(map(mul, (abs(dir[1]), abs(dir[1])), dir_1))
+                dir = tuple(map(add, dir_0, dir_1))
+    return abs(pos[0]) + abs(pos[1])
 
 
 print('The answer to part 1 is: {}'.format(mvstp('move_ship', (0, 0), (1, 0))))
+print('The answer to part 2 is: {}'.format(mvstp('move_wp', (0, 0), (10, 1))))
