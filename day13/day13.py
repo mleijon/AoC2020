65ea7d9fdf56bb9757b5da2507277413446f6a53
mleijon <36968756+mleijon@users.noreply.github.com>
2020-12-18 12:08:39 +0100

    Day 13b in progress!

--- day13/day13.py
+++ day13/day13.py
@@ -1,16 +1,20 @@
-with open('p13_input.txt') as fi:
+with open('test.txt') as fi:
     teststr, datastr = fi.readlines()
 data = datastr.split(',')
 testnr = int(teststr)
 buses = []
+count = -1
 for item in data:
     try:
-        buses.append(int(item))
+        buses.append((int(item), count + 1))
+        count += 1
     except:
+        count += 1
         continue
 waittime = 100000
 store = tuple()
-for item in buses:
+print(buses)
+for item in [x[0] for x in buses]:
     if item - testnr % item < waittime:
         waittime = item - testnr % item
         store = item, waittime
