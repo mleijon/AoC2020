330add55fc9513b83af9527559950624eba20c19
mleijon <mikael.leijon@gmail.com>
2020-12-30 08:22:06 +0100

    Day13b in progress!

--- day13/day13mp.py
+++ day13/day13mp.py
@@ -2,21 +2,24 @@ import multiprocessing as mp
 
 
 def runcheck(n):
-    buses = n[1:]
-    n = n[0]
-    for i in range(len(buses)):
-        if (n + buses[i + 1][1] - buses[0][1]) % buses[i + 1][0] != 0:
-            break
-        elif i == len(buses) - 2:
-            print('The answer to part 2 is: {}'.format(n - buses[0][1]))
-            return True
-        else:
-            continue
+    buses = n[1]
+    data = n[0]
+    for item in data:
+        for i in range(len(buses)):
+            if (item + buses[i + 1][1] - buses[0][1]) % buses[i + 1][0] != 0:
+                break
+            elif i == len(buses) - 2:
+                print('The answer to part 2 is: {}'.format(item - buses[0][1]))
+                return True
+            else:
+                if i == len(buses) - 4:
+                    print(item)
+                continue
     return False
 
 
 if __name__ == '__main__':
-    with open('test.txt') as fi:
+    with open('p13_input.txt') as fi:
         teststr, datastr = fi.readlines()
     data = datastr.split(',')
     testnr = int(teststr)
@@ -39,11 +42,18 @@ if __name__ == '__main__':
     print('The answer to part 1 is: {}'.format(store[0] * store[1]))
 
     n = 0
-    while True:
 
-        n += mp.cpu_count() * buses[0][0]
-        jobs = [n - x*buses[0][0] for x in range(mp.cpu_count())]
-        jobs = [[x] + buses for x in jobs]
+    mult = 1000000
+    m = mult * mp.cpu_count()
+    while True:
+        n += 1
+        jobs_all = [(n*m - x)*buses[0][0] for x in range(m)]
+        jobs = list()
+        for x in range(mp.cpu_count()):
+            job = list()
+            job.append(jobs_all[x * mult:(x + 1) * mult])
+            job.append(buses)
+            jobs.append(job)
         with mp.Pool(mp.cpu_count()) as p:
             if True in p.map(runcheck, jobs):
                 p.terminate()
