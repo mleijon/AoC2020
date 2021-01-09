0345f2d42ca15f9ba349443c259c573ed18a2eb5
mleijon <mikael.leijon@gmail.com>
2021-01-09 16:16:34 +0100

    Day14 part 1 done!

--- day14/day14.py
+++ day14/day14.py
@@ -1,3 +1,29 @@
+data = list()
+mem = dict()
 with open('p14_input.txt')  as fi:
     for item in fi.readlines():
-        print(item)
+        if item[:4] == 'mask':
+            mask_a = ''
+            mask_b = ''
+            current_mask = item.split('mask = ')[1].strip()
+            for ch in current_mask:
+                if ch == 'X':
+                    mask_a += '1'
+                    mask_b += '0'
+                elif ch == '0':
+                    mask_a += '0'
+                    mask_b += '0'
+                else:
+                    mask_a += '0'
+                    mask_b += '1'
+        else:
+            number = int(item.split(' = ')[1].strip())
+            number = int(mask_a, 2) & number
+            number = int(mask_b, 2) ^ number
+            adr = (item.split(' = ')[0].strip()).replace(']', '[').split('[')[1]
+            mem[adr] = number
+sum_all = 0
+for key in mem.keys():
+    sum_all += mem[key]
+print('The answer to part 1 is: {}'.format(sum_all))
+
