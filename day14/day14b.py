aa505724bb6d11b4245bb7b60b932e2f9c52c0ff
mleijon <mikael.leijon@gmail.com>
2021-01-10 00:27:56 +0100

    Day14 done!

--- day14/day14b.py
+++ day14/day14b.py
@@ -1,8 +1,5 @@
-mem = dict()
-masks = {''}
-
-
-def createxmasks(mask, masks):
+def createxmasks(mask):
+    masks = {''}
     for ch in mask:
         tmp_masks = set()
         for item in masks:
@@ -21,27 +18,29 @@ def createxmasks(mask, masks):
     return masks
 
 
-createxmasks('X0000000000011001111011101111011101X', masks)
+mem = dict()
 
 with open('p14_input.txt') as fi:
     for item in fi.readlines():
         if item[:4] == 'mask':
-            mask_a = mask_b = ''
             current_mask = item.split('mask = ')[1].strip()
-            for ch in current_mask:
-                if ch == 'X':
-                    mask_a += '1'
-                    mask_b += '0'
-                elif ch == '0':
-                    mask_a += '0'
-                    mask_b += '0'
-                else:
-                    mask_a += '0'
-                    mask_b += '1'
         else:
-            number = int(item.split(' = ')[1].strip())
-            number = int(mask_a, 2) & number
-            number = int(mask_b, 2) ^ number
-            adr = (item.split(' = ')[0].strip()).replace(']', '[').split('[')[1]
-            mem[adr] = number
-print('The answer to part 1 is: {}'.format(sum(mem.values())))
+            for m in createxmasks(current_mask):
+                mask_a = mask_b = ''
+                for ch in m:
+                    if ch == 'X':
+                        mask_a += '1'
+                        mask_b += '0'
+                    elif ch == '0':
+                        mask_a += '0'
+                        mask_b += '0'
+                    else:
+                        mask_a += '0'
+                        mask_b += '1'
+                adr = int((item.split(' = ')[0].strip()).replace(']', '[').
+                          split('[')[1])
+                adr = int(mask_a, 2) & adr
+                adr = int(mask_b, 2) ^ adr
+                number = int(item.split(' = ')[1].strip())
+                mem[adr] = number
+print('The answer to part 2 is: {}'.format(sum(mem.values())))
