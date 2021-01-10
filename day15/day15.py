ce4e0a3c14f557e5602bfdc20db72384d4666d6b
mleijon <mikael.leijon@gmail.com>
2021-01-10 23:44:29 +0100

    Day15 done!

--- day15/day15.py
+++ day15/day15.py
@@ -1,11 +1,11 @@
 turns = {16: (1, 1), 1: (2, 2), 0: (3, 3), 18: (4, 4), 12: (5, 5), 14: (6, 6)}
 turn_nr = len(turns) + 1
 current_key = 19
-while turn_nr < 2020:
+while turn_nr < 30000000:
     if current_key in turns.keys():
         turns[current_key] = (turn_nr, turns[current_key][0])
     else:
         turns.update({current_key: (turn_nr, turn_nr)})
     turn_nr += 1
     current_key = turns[current_key][0] - turns[current_key][1]
-print('The answer to part 1 is: {}'.format(current_key))
+print('The answer to part 2 is: {}'.format(current_key))
